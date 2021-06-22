8038d49fbf5f8790103e3857db68515ac4ddccc9
Eduardo-Azuolas <eduardoazuolas@gmail.com>
2021-06-22 14:28:19 -0300

    segundo passo na aplicação: começando o DB, rotas e controllers

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
-import express from "express"
+import "reflect-metadata";
+import express from "express";
+import { router } from "./routes"
+import "./database";
 
 //@types/express
 const app = express()
+
+app.use(express.json())
+
+app.use(router)
 
 /**
  *  GET => Buscar informação
@@ -11,15 +18,15 @@ const app = express()
  *  PATCH => Alterar uma informação específica
 */
 
-app.get("/test", (request, response) =>{
-    // Request => Entrando
-    //Response => Saindo
-    return response.send("Olá NLW")
-})
-
-app.post("/test-post", (request,response) => {
-    return response.send("Olá NLW método POST")
-})
+/**
+ * Tipos de Parâmetro
+ * Routes Params => http://localhost:3000/produtos/237427365
+ * Query Params (não obrigatórios - utilizados pra filtro) => http://localhost:3000/produtos?name=teclado&description=tecladobom&
+ * Body Params (utilizado em POST,PUT e PATCH)=> {
+ *  "name": "teclado",
+ *  "description": "teclado bom"
+ * }
+ */
 
 //http://localhost:3000
 app.listen(3000, () => console.log("Server is running"))
