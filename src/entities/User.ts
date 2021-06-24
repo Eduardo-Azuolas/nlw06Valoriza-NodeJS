f1788c5add61479ea67ad8940b87806a67fcbf3c
Eduardo-Azuolas <eduardoazuolas@gmail.com>
2021-06-24 16:56:01 -0300

    Quarto passo NWL06-NodeJS: implementação de JWT, autenticação de usuário, estrutura de migrations, entity, repositories, services e controllers

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -5,14 +5,22 @@ import { v4 as uuid } from "uuid"
 class User {
     @PrimaryColumn()
     readonly id: string;
+
     @Column()
     name: string;
+
     @Column()
     email: string;
+
     @Column()
     admin: boolean;
+
+    @Column()
+    password: string;
+
     @CreateDateColumn()
     created_at: Date;
+
     @UpdateDateColumn()
     updated_at: Date;
 
